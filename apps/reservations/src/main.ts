c8133f383b55bf2ddf8b278d69d512c4bd188214
Kheder Taleb <khdertaleb1@gmail.com>
2025-09-13 23:18:40 +0300

    feat: add auth project configuration and update logging in reservations module

--- apps/reservations/src/main.ts
+++ apps/reservations/src/main.ts
@@ -4,9 +4,11 @@ import { ReservationsModule } from './reservations.module';
 import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
+  console.log(process.env.MONGODB_URI);
+  console.log('Hello!');
   const app = await NestFactory.create(ReservationsModule);
   app.useGlobalPipes(new ValidationPipe({ whitelist: true }));
   app.useLogger(app.get(Logger));
-  await app.listen(process.env.port ?? 5000);
+  await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
